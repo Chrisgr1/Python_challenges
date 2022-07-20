079f5a7867ce3f07ebd4d3a3d02cd972d003c931
Chris <chris.gr.skinner@gmail.com>
2022-07-20 21:14:32 +0100

    task set

---
+++ Python/Challenges/control_flow.py
@@ -0,0 +1,8 @@
+# From Codecademy Beginner Python advanced challenges
+#1 Create a function named in_range() that has three parameters named num, lower, and upper.The function should return True if num is greater than or equal to lower and less than or equal to upper. Otherwise, return False.
+#2 Create a function named same_name() that has two parameters named your_name and my_name. If our names are identical, return True. Otherwise, return False.
+#3 Create a function named always_false() that has one parameter named num.Using an if statement, your variable num, and the operators >, and <, make it so your function will return False no matter what number is stored in num. An if statement that is always false is called a contradiction. You will rarely want to do this while programming, but it is important to realize it is possible to do this.
+#4 Create a function named movie_review() that has one parameter named rating.If rating is less than or equal to 5, return "Avoid at all costs!". If rating is between 5 and 9, return "This one was fun.". If rating is 9 or above, return "Outstanding!"
+#5 Create a function called max_num() that has three parameters named num1, num2, and num3. The function should return the largest of these three numbers. If any of two numbers tie as the largest, you should return "It's a tie!".
+
+#1 
