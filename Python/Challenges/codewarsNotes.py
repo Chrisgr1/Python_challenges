eb9211cc108bcc2fb740ef75040441b5919d61da
Chris Skinner <95317377+Chrisgr1@users.noreply.github.com>
2022-08-03 18:22:29 +0100

    mo python, mo problems - nice to review mutable sequence types

--- Python/Challenges/codewarsNotes.py
+++ Python/Challenges/codewarsNotes.py
@@ -1,8 +1,87 @@
-def find_smallest_int(arr):
-    return sorted(arr)[0]
+import string
 
-print (find_smallest_int([78, 56, 232, 12, 11, 43]))
-print (find_smallest_int([0, 1-2**64, 2**64]))
+
+def filter_list(lst):
+    newlist = []
+    for item in lst:
+                if isinstance(item, str) == False:
+                    newlist.append(item)
+    return newlist
+
+
+print(filter_list([1, 2, 'a', 'b']), [1, 2])
+
+# def solution(string):
+#     return string[::-1]
+# print (solution('world'), 'dlrow')
+
+# def reverse_words(text):
+#     return text[::-1]
+
+# print(reverse_words("gab llab"))
+
+# def greet():
+#     return ("hello world!")
+
+# print(greet())
+# def litres(time):
+#     return int(time/2)
+
+# print(litres(3))
+
+
+# def lovefunc(a,b):
+#     return a%2 == 0 and b%2 !=0 or b%2 == 0 and a%2 !=0
+
+# print (lovefunc(1,2))
+# def to_jaden_case(string):
+    
+#     for s in string:
+#         return string.title()
+
+# quote = "How can mirrors be real if our eyes aren't real"
+# print(to_jaden_case(quote))
+
+
+# def friend(fre):
+#     newfriends=[]
+#     for f in fre:
+#         if len(f) == 4:
+#             newfriends.append(f)
+#     return newfriends
+
+# print(friend(["Ryan", "Kieran", "Mark"]))
+# print(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]))
+# print(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]))
+
+# def bool_to_word(boolean):
+#         if boolean == False:
+#             return "No"
+#         if boolean == True:
+#             return "Yes"
+
+
+# print(bool_to_word(True))
+# print(bool_to_word(False))
+
+# def positive_sum(arr):
+#     if arr == None or []:
+#         return 0
+#     newarr = []
+#     for number in arr:
+#         if number > 0:
+#             newarr.append(number)
+#     if newarr == []:
+#         return 0
+#     return sum(newarr)
+
+# print(positive_sum([-1,2,3,4,-5]))
+
+# def find_smallest_int(arr):
+#     return sorted(arr)[0]
+
+# print (find_smallest_int([78, 56, 232, 12, 11, 43]))
+# print (find_smallest_int([0, 1-2**64, 2**64]))
 
 # def sum_two_smallest_numbers(numbers):
 #     return sorted(numbers)[0] + sorted(numbers)[1]
